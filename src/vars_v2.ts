e696826639e9494e1d7ab892aeb4b692040ced6e
xjq熊剑桥 <jqxiong@Ctrip.com>
2017-07-04 18:13:28 +0800

    update

--- src/vars_v2.ts
+++ src/vars_v2.ts
@@ -9,6 +9,17 @@
 // 	setTimeout(() => console.log(i), 30);
 // }
 
-const FETCH_LIST = 'CAR/FETCH_LIST';
+// const FETCH_LIST: string = 'CAR/FETCH_LIST';
+// 会报错, const类型的值不能修改
+// FETCH_LIST = 'OCH/';
 
-FETCH_LIST = 'OCH/'
+// 解构
+enum obj {cid = 108, cnm = 'shanghai'};
+
+let {cid, cnm} = obj;
+console.log(cid);
+
+let arr1: Array<number> = [3, 4, 5];
+
+let arr2 = [1, 2, ...(arr1 as Array<number>), 6];
+console.log(arr2);
