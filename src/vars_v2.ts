a075bad35931ac1924ce0c5d0bae3e09dea552f0
xjq熊剑桥 <jqxiong@Ctrip.com>
2017-07-04 16:04:37 +0800

    update

---
+++ src/vars_v2.ts
@@ -0,0 +1,14 @@
+// 变量的声明与解构(var let const)
+
+// var 与 let的经典区别
+// for (var i = 0; i < 10; i++) {
+// 	setTimeout(() => console.log(i), 30);
+// }
+
+// for (let i = 0; i < 10; i++) {
+// 	setTimeout(() => console.log(i), 30);
+// }
+
+const FETCH_LIST = 'CAR/FETCH_LIST';
+
+FETCH_LIST = 'OCH/'
