8b0b5b448d733bc142dde6b149473209bf58b565
xjq熊剑桥 <jqxiong@Ctrip.com>
2017-07-06 20:47:23 +0800

    class

--- src/class.ts
+++ src/class.ts
@@ -30,4 +30,30 @@ class Man extends Human {
 }
 
 let bear = new Man('xiongxiong', 1899);
-console.log(bear.power);
+// console.log(bear.power);
+
+// 公有、私有和受保护
+class BaseView {
+	// pageid // 私有变量, 派生类中不可访问
+	private _pageToken: number = 12315;
+	protected pageType: string = 'layer';
+	// 页面状态
+	state: object;
+	// initial
+	constructor(initState?: object) {
+		this.state = initState;
+	}
+	// 可以通过public方法访问私有变量, 但是无法直接访问私有变量
+	showToken() {
+		console.log(this._pageToken);	
+	}
+}
+
+class UILayerView extends BaseView {
+	constructor(state?: object) {
+		super(state);
+		this.showToken();
+	}
+}
+
+let UIlayer = new UILayerView();
